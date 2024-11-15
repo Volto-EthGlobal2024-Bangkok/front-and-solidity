69368a97e660e256d9fc1e482b57d747d6ceecf4
Stefan <st.nikolic01@gmail.com>
2024-11-16 04:46:02 +0700

    feat: init theme

--- Volto/packages/nextjs/app/page.tsx
+++ Volto/packages/nextjs/app/page.tsx
@@ -3,7 +3,15 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl">Hello World!</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
